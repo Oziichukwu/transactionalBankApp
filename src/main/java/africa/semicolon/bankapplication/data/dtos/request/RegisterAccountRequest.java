da4651a49396d3bb1beb795516d0084db6fc403d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-06 21:22:11 +0100

    account request and response dtos

--- src/main/java/africa/semicolon/bankapplication/data/dtos/request/RegisterAccountRequest.java
+++ src/main/java/africa/semicolon/bankapplication/data/dtos/request/RegisterAccountRequest.java
@@ -1,2 +1,17 @@
-package africa.semicolon.bankapplication.data.dtos.request;public class RegisterAccountRequest {
+package africa.semicolon.bankapplication.data.dtos.request;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class RegisterAccountRequest {
+
+    private String accountName;
+
+    private String accountPassword;
+
+    private double initialDeposit;
 }
--- src/main/java/africa/semicolon/bankapplication/data/dtos/response/RegisterAccountResponse.java
+++ src/main/java/africa/semicolon/bankapplication/data/dtos/response/RegisterAccountResponse.java
@@ -1,2 +1,16 @@
-package africa.semicolon.bankapplication.data.dtos.response;public class RegisterAccountResponse {
+package africa.semicolon.bankapplication.data.dtos.response;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RegisterAccountResponse {
+
+    private boolean success;
+
+    private String message;
 }
