cd02236434ff927ac18f92d6d79cd430d9ebc487
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-06 21:21:33 +0100

    transaction and account model updated

--- src/main/java/africa/semicolon/bankapplication/data/models/Account.java
+++ src/main/java/africa/semicolon/bankapplication/data/models/Account.java
@@ -1,2 +1,33 @@
-package africa.semicolon.bankapplication.data.models;public class Account {
+package africa.semicolon.bankapplication.data.models;
+
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    private String accountNumber;
+
+    private String accountName;
+
+    private String accountPassword;
+
+    private String accountBalance;
+
+    private List<Transaction>transactions;
+
+    public void addTransaction(Transaction transaction){
+        if (transactions == null){
+            transactions = new ArrayList<>();
+        }
+        transactions.add(transaction);
+    }
+
 }
--- src/main/java/africa/semicolon/bankapplication/data/models/Transaction.java
+++ src/main/java/africa/semicolon/bankapplication/data/models/Transaction.java
@@ -1,2 +1,22 @@
-package africa.semicolon.bankapplication.data.models;public class Transaction {
+package africa.semicolon.bankapplication.data.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+@Data
+@Builder
+public class Transaction {
+
+    private LocalDate transactionDate;
+
+    private String transactionType;
+
+    private String narration;
+
+    private Double amount;
+
+    private Double accountBalance;
 }
