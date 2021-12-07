dc1d3e752dc740e47605c4f68f258553d2f46f12
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-07 22:56:12 +0100

    account service interface

--- src/main/java/africa/semicolon/bankapplication/services/AccountService.java
+++ src/main/java/africa/semicolon/bankapplication/services/AccountService.java
@@ -1,5 +1,21 @@
 package africa.semicolon.bankapplication.services;
 
+import africa.semicolon.bankapplication.data.dtos.request.DepositRequest;
+import africa.semicolon.bankapplication.data.dtos.request.WithdrawRequest;
+import africa.semicolon.bankapplication.data.dtos.response.AccountInfo;
+import africa.semicolon.bankapplication.data.dtos.response.TransactionResponse;
+import africa.semicolon.bankapplication.data.models.Transaction;
+
+import java.util.List;
+
 public interface AccountService {
+
+    TransactionResponse withdraw(WithdrawRequest request);
+
+    TransactionResponse deposit(DepositRequest request);
+
+    AccountInfo getAccountInfo(String accountNumber, String password);
+
+    List<Transaction> getAllAccountTransaction(String accountNumber);
 
 }
