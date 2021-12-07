9978e9e56196c8ece2753b23fd440cd5c437d3d2
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-07 05:14:45 +0100

    exception

--- src/main/java/africa/semicolon/bankapplication/data/exceptions/BankApplicationException.java
+++ src/main/java/africa/semicolon/bankapplication/data/exceptions/BankApplicationException.java
@@ -1,2 +1,18 @@
-package africa.semicolon.bankapplication.data.exceptions;public class BankApplicationException {
+package africa.semicolon.bankapplication.data.exceptions;
+
+public class BankApplicationException extends RuntimeException{
+    public BankApplicationException() {
+    }
+
+    public BankApplicationException(String message) {
+        super(message);
+    }
+
+    public BankApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BankApplicationException(Throwable cause) {
+        super(cause);
+    }
 }
--- src/main/java/africa/semicolon/bankapplication/data/exceptions/InsufficientInitialDepositException.java
+++ src/main/java/africa/semicolon/bankapplication/data/exceptions/InsufficientInitialDepositException.java
@@ -1,2 +1,18 @@
-package africa.semicolon.bankapplication.data.exceptions;public class InsufficientInitialDepositException {
+package africa.semicolon.bankapplication.data.exceptions;
+
+public class InsufficientInitialDepositException extends BankApplicationException {
+    public InsufficientInitialDepositException() {
+    }
+
+    public InsufficientInitialDepositException(String message) {
+        super(message);
+    }
+
+    public InsufficientInitialDepositException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InsufficientInitialDepositException(Throwable cause) {
+        super(cause);
+    }
 }
