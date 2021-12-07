9b2407c1181fa8d848f3c9acf0ef9afdcf444008
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-07 23:35:47 +0100

    accountinfo dto added

--- src/main/java/africa/semicolon/bankapplication/data/dtos/response/AccountInfo.java
+++ src/main/java/africa/semicolon/bankapplication/data/dtos/response/AccountInfo.java
@@ -1,6 +1,18 @@
 package africa.semicolon.bankapplication.data.dtos.response;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class AccountInfo {
 
+    private String accountName;
+
+    private String accountNumber;
+
+    private Double balance;
 
 }
