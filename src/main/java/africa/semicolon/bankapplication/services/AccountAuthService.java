ac32131071922d919028e9996672d43c972c6c8b
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-07 06:49:46 +0100

    account service

--- src/main/java/africa/semicolon/bankapplication/services/AccountAuthService.java
+++ src/main/java/africa/semicolon/bankapplication/services/AccountAuthService.java
@@ -2,8 +2,16 @@ package africa.semicolon.bankapplication.services;
 
 import africa.semicolon.bankapplication.data.dtos.request.RegisterAccountRequest;
 import africa.semicolon.bankapplication.data.dtos.response.RegisterAccountResponse;
+import africa.semicolon.bankapplication.data.models.Account;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AccountAuthService {
     RegisterAccountResponse registerAccount(RegisterAccountRequest registerAccountRequest);
+
+    List<Account> findAllAccounts();
+
+    Optional<Account>findAccountByAccountNumber(String accountNumber);
 
 }
