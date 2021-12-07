d73366eb61d12bb111495c62d0f62ee928dabf35
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-07 23:18:26 +0100

    deposit request dto

--- src/main/java/africa/semicolon/bankapplication/data/dtos/request/DepositRequest.java
+++ src/main/java/africa/semicolon/bankapplication/data/dtos/request/DepositRequest.java
@@ -1,5 +1,15 @@
 package africa.semicolon.bankapplication.data.dtos.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class DepositRequest {
 
+    private String accountNumber;
+
+    private Double amount;
 }
